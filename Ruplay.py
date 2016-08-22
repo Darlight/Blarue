2aad6688e433dc25ce0351d5a37e7ea2f005a31f
Darlight <andres.perdomo09@gmail.com>
2016-08-22 08:49:09 -0600

    Inicio del proyecto rurplay

--- Ruplay.py
+++ Ruplay.py
@@ -1,3 +1,4 @@
+#Niveles o plataformas con diferentes objetivos
 def cargar_mapa(nombre):
 	mapa = open(nombre, "r")
 	lineas = []
@@ -6,4 +7,14 @@ def cargar_mapa(nombre):
 	return lineas
 
 print(cargar_mapa("mapas/mapa1.txt"))
+
+#Instrucciones(programa por default para ganar un mapa)
+def cargar_instrucciones(nombre):
+	instruccion = open(nombre, "r")
+	lineas_2 = []
+	for linea in instruccion:
+		lineas_2.append(linea.strip())
+	return lineas_2
+
+print(cargar_instrucciones("Instrucciones/instruccion1.txt"))
 

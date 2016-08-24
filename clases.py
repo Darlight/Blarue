d43bf905c50dc55398e7db90d10aa549298a7abf
Darlight <andres.perdomo09@gmail.com>
2016-08-24 08:49:14 -0600

    preguntarle al profesor una idea o recordar el sentido del mapa

--- clases.py
+++ clases.py
@@ -1,6 +1,14 @@
 class Mapa(object):
 	def __init__(self, altura, ancho, fichas, robot):
-		self.altura = ""
-		self.ancho = ""
-		self.fichas = ""
+		self.altura = 0
+		self.ancho = 0
+		self.fichas = 0
 		self.robot = robot
+
+	def ubicar_robot(self):
+		for altura in range(len(mapalista))
+			for ancho in range(len(mapalista[ancho]))
+				if mapa_lista[ancho][altura] == "▲"
+
+	def imprimir_mapa(self):
+		
--- robots.py
+++ robots.py
@@ -4,21 +4,28 @@ class Robot(object):
 		self.rotacion = 1
 		self.movimiento = 0
 		self.pick = False
-		self.posicion_y = ""
-		self.posicion_x = ""
+		self.posicion_y = 0
+		self.posicion_x = 0
 		self.mapa = mapa 
 		self.fichas = 0
+
 	def rotar_posicion(self):
-		if self.rotacion = 1:
-			return chr(176)
+		self.rotacion = 1
+		if self.rotacion >= 5:
+			self.rotacion = 1
+		elif self.rotacion = 1:
+			return "▲"
 		elif self.rotacion = 2:
-			return chr()
+			return "►"
 		elif self.rotacion = 3:
-			return chr()
+			return "▼"
 		elif self.rotacion = 4:
-			return chr()
+			return "◄"
+	
 	def recoger_ficha(self):
-		
+		if hay_ficha(self.posicion_x, self.posicion_y):
+			mapa.recoger_ficha(self.x, self.y)
+			self.fichas += 1
 
 
 	
